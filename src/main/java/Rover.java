8c12d8ca4b91357203da483fdfd397aaaf0fb568
SirXOPHER <chris.reinmuller@gmail.com>
2018-07-13 14:36:42 +0100

    Pass test 'exposeInitialLocation()'

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,12 +1,18 @@
 public class Rover {
 
+    private int[] location;
     private String heading;
 
-    public Rover(String heading) {
+    public Rover(int[] location, String heading) {
+        this.location = location;
         this.heading = heading;
     }
 
     public String getHeading() {
         return this.heading;
+    }
+
+    public int[] getLocation() {
+        return location;
     }
 }
